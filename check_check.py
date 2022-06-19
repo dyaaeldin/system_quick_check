bd33806e7b90427e6ce8929d4216a9cfb9325846
Dyaa Ahmed <79382618+dyaaeldin@users.noreply.github.com>
2022-06-19 07:12:14 +0200

    Create check_check.py

---
+++ check_check.py
@@ -0,0 +1,51 @@
+import requests
+import psutil
+import os
+import socket
+
+mem_threshold=70
+cpu_threshold=70
+disk_threshold=70
+
+def check_disk_status():
+    disk_status=psutil.disk_usage("/").percent
+    print("Disk usage:", disk_status)
+    return disk_status > disk_threshold
+
+def check_internet_connectivity():
+    conn=requests.get("https://google.com")
+    print("internet connectivity is ok")
+    return conn.status_code != 200
+
+def get_public_ip():    
+    ext_ip=os.popen('curl -s ifconfig.me').readline()
+    print("Public IP:", ext_ip)
+
+def check_the_cpu_usage():
+    l1, l2, l3 = psutil.getloadavg()
+    cpu_usage = (l3/os.cpu_count()) * 100
+    print("CPU usage:", cpu_usage)
+    return cpu_usage > cpu_threshold
+
+def check_mem_usage():
+    mem_usage = 100 - psutil.virtual_memory().available * 100 / psutil.virtual_memory().total
+    print ("MEM usage:", mem_usage)
+    return mem_usage > mem_threshold 
+
+def ip_info():
+    hostname=socket.gethostname()
+    ip_addr=socket.gethostbyname(hostname)
+    print("Priv IP:", ip_addr)
+
+def print_hashes(word):
+    print(f"====={word}=======")
+
+def main():  
+    if check_internet_connectivity() or check_disk_status() or check_the_cpu_usage() or check_mem_usage():
+        print("Some thing went wrong please check your sytem")
+    else:
+        print_hashes("ip")
+        ip_info()
+        get_public_ip()
+        print_hashes("==")
+main()
